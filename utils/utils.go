9affdf8ec8c6f7e2ed7e5f25124dbc4d4f95f4e6
IrvinTM <irvint698@gmail.com>
2024-08-04 21:35:12 -0600

    updated gen function

--- utils/utils.go
+++ utils/utils.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"math/rand"
 	"net/http"
+	"strings"
 )
 
 func Message(status bool, message string) map[string]interface{} {
@@ -16,11 +17,11 @@ func Respond(w http.ResponseWriter, data map[string]interface{}) {
 	json.NewEncoder(w).Encode(data)
 }
 
-func GenShort() string{
-	var chars = []string {"a", "b", "c", "d", "e", "f", "g", "h", "i","j", "k", "l", "m", "n", "o", "p", "q", "r","s", "t", "v", "w", "x", "y", "z", "1", "2","3", "4", "5", "6", "7", "8", "9", "0"}
-	random := ""
-	for i := 0; i < 7; i++ {
-		random += chars[rand.Intn(len(chars))]
-	}
-	return random
+func GenShort() string {
+    const chars = "abcdefghijklmnopqrstuvwxyz1234567890"
+    var builder strings.Builder
+    for i := 0; i < 7; i++ {
+        builder.WriteByte(chars[rand.Intn(len(chars))])
+    }
+    return builder.String()
 }
