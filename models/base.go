6f93a4d0582c5b7c65e502a3935415c679c4e090
IrvinTM <irvint698@gmail.com>
2024-08-14 15:35:59 -0600

    env fix

--- models/base.go
+++ models/base.go
@@ -1,41 +1,51 @@
 package models
 
 import (
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/jinzhu/gorm"
-	"os"
-	"github.com/joho/godotenv"
 	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-var db *gorm.DB //database
+var db *gorm.DB 
 
 func init() {
+	// Fetch environment variables 
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
 
-	e := godotenv.Load() //Load .env file
-	if e != nil {
-		fmt.Print(e)
+	if username == "" || password == "" || dbName == "" || dbHost == "" {
+		log.Fatal("One or more environment variables (DB_USER, DB_PASS, DB_NAME, DB_HOST) are missing")
 	}
 
-	username := os.Getenv("db_user")
-	password := os.Getenv("db_pass")
-	dbName := os.Getenv("db_name")
-	dbHost := os.Getenv("db_host")
+	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password)
+	fmt.Println("Connecting to the database...")
 
+	// Retry
+	var err error
+	for i := 0; i < 5; i++ { 
+		db, err = gorm.Open("postgres", dbUri)
+		if err == nil {
+			log.Println("Successfully connected to the database.")
+			break
+		}
+		log.Printf("Failed to connect to database (attempt %d): %v", i+1, err)
+		time.Sleep(5 * time.Second) 
+	}
 
-	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
-	// fmt.Println(dbUri)
-
-	conn, err := gorm.Open("postgres", dbUri)
 	if err != nil {
-		fmt.Print(err)
+		log.Fatalf("Could not connect to the database: %v", err)
 	}
 
-	db = conn
-	db.Debug().AutoMigrate(&Account{}, &Url{}) //Database migration
+	db.Debug().AutoMigrate(&Account{}, &Url{}) // Database migration
 }
 
-//returns a handle to the DB object
+
 func GetDB() *gorm.DB {
 	return db
 }
